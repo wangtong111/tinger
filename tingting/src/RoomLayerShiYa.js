8b56de57ea45e48187203f5804e8b6b117500420
tongtong <1055768118@qq.com>
2017-11-25 15:15:24 +0800

    施压实验

---
+++ tingting/src/RoomLayerShiYa.js
@@ -0,0 +1,38 @@
+/**
+ * Created by wangtong on 2017/11/25.
+ */
+
+var RoomLayerShiYa = RoomLayer.extend({
+
+    initOwnBg : function(node){
+        var self = this;
+
+        var menu = new cc.Menu();
+        menu.setPosition(0,0);
+        node.addChild(menu,10);
+
+        function onTouch(tag,sender){
+            var scene = new PlayScene(self.types,tag);
+            cc.director.pushScene(scene);
+        }
+
+        var flagConfig = FLAG_CONFIG[self.types];
+        self.lev =  self.lev >  flagConfig.length ? flagConfig.length : self.lev;
+
+        for(var i = 0 ; i< self.lev ; i++){
+            var spNormal = new cc.Sprite(res.room_flag_png);
+            var spSelect = new cc.Sprite(res.room_flag_png);
+
+            var buyItem = new cc.MenuItemSprite(spNormal,spSelect);
+            buyItem.setPosition(flagConfig[i]);
+            buyItem.setAnchorPoint(0.5,0.5);
+            buyItem.setCallback(onTouch.bind(this,i+ 1),this);
+            menu.addChild(buyItem,1);
+            buyItem.setOpacity(0);
+
+            buyItem.runAction(cc.sequence(cc.delayTime(i*0.2),cc.fadeIn(0.2)));
+        }
+    }
+
+
+}) ;
